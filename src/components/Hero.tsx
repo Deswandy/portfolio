10c4312933f0aed25ff319a3913bc4d2d7807b59
Deswandy <deswandywong23@gmail.com>
2025-11-11 09:49:02 +0700

    feat: landing 50%

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import roof from "@/assets/images/roof2.png";
+import hero from "@/assets/images/swork-landing.png"
 
 const Hero = () => {
   return (
@@ -8,7 +9,14 @@ const Hero = () => {
         className="absolute -left-8 w-[200vw] sm:w-[120vw] max-w-none z-20"
       />
 
-      <div className="h-[80vh] bg-accent-foreground border-4 sm:border-8 border-accent"></div>
+      <div className="flex flex-row gap-18 items-center justify-center h-[84vh] bg-accent-foreground border-4 sm:border-8 border-accent">
+        <div className="flex flex-col gap-2 mt-10 w-1/2">
+          <h1 className="text-8xl text-wrap w-full font-black text-accent">The Fullstack RESTaurant</h1>
+          <h1 className="text-3xl text-wrap w-full font-medium text-accent">Cooking up solutions, one byte at a thyme</h1>
+          <h1 className="text-xl text-wrap w-full font-medium text-secondary">A Portfolio by Wandy</h1>
+        </div>
+        <img src={hero} className="pt-10 h-[70%]"/>
+      </div>
     </section>
   );
 };
