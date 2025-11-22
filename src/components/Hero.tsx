bfd7c80fa1b0b306718000af79c6f910e24aad7f
Deswandy <deswandywong23@gmail.com>
2025-11-22 17:16:20 +0700

    feat: responsive hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,28 @@ import hero from "@/assets/images/swork-landing.png";
 import { ArrowDown } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 import SplitText from "@/components/SplitText";
+import { useEffect, useState } from "react";
 // import PixelBlast from "./PixelBlast";
 const Hero = () => {
+  type TextAlign = "start" | "center" ;
+  const [textAlign, setTextAlign] = useState<TextAlign>("center");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 640) {
+        setTextAlign("center");
+      } else {
+        setTextAlign("start");
+      }
+    };
+
+    handleResize(); // run on mount
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <section className="relative w-full overflow-x-clip">
-
       {/* Roof image on top */}
       <img
         src={roof}
@@ -15,8 +32,7 @@ const Hero = () => {
       />
 
       {/* Main hero block */}
-      <div className="relative flex flex-row gap-18 items-center justify-center h-[84vh] bg-accent-foreground border-4 sm:border-8 border-accent overflow-hidden">
-
+      <div className="relative flex flex-col-reverse sm:flex-row gap-4 sm:gap-18 items-center justify-center h-full sm:h-[84vh] bg-accent-foreground border-4 sm:border-8 border-accent overflow-hidden">
         {/* PixelBlast = background layer */}
         {/* <div className="absolute inset-0 z-0 pointer-events-none opacity-25">
           <PixelBlast
@@ -32,8 +48,8 @@ const Hero = () => {
         </div> */}
 
         {/* Foreground content */}
-        <div className="flex flex-col gap-2 mt-10 w-1/2 z-20">
-          <h1 className="text-4xl md:text-6xl lg:text-8xl w-full font-black text-accent ">
+        <div className="flex flex-col gap-0 sm:gap-2 sm:mt-10 px-6 sm:w-1/2 z-20">
+          <h1 className="text-center sm:text-start text-4xl sm:text-6xl lg:text-8xl w-full font-black text-accent ">
             <SplitText
               text="The Fullstack RESTaurant"
               delay={100}
@@ -43,20 +59,20 @@ const Hero = () => {
               from={{ opacity: 0, y: 40 }}
               to={{ opacity: 1, y: 0 }}
               threshold={0.1}
-              textAlign="start"
+              textAlign={textAlign}
             />
           </h1>
 
-          <h1 className="text-3xl font-medium text-accent">
+          <h1 className="text-xl text-center sm:text-start sm:text-3xl font-medium text-accent">
             Cooking up solutions, one byte at a thyme
           </h1>
 
-          <h1 className="text-xl font-medium text-secondary">
+          <h1 className="text-sm text-center sm:text-start pb-4 sm:pb-0 sm:text-xl font-medium text-secondary">
             A Portfolio by Wandy
           </h1>
         </div>
 
-        <img src={hero} className="pt-10 h-[70%] z-20" />
+        <img src={hero} className="pt-16 sm:pt-10 h-64 sm:h-[70%] z-20" />
       </div>
 
       {/* Tooltip button - also above background */}
@@ -65,9 +81,11 @@ const Hero = () => {
           <button
             aria-label="Scroll down"
             onClick={() =>
-              document.getElementById("profile")?.scrollIntoView({ behavior: "smooth" })
+              document
+                .getElementById("profile")
+                ?.scrollIntoView({ behavior: "smooth" })
             }
-            className="absolute bottom-5 left-1/2 -translate-x-1/2 animate-bounce hover:cursor-pointer z-30"
+            className="hidden sm:grid absolute bottom-5 left-1/2 -translate-x-1/2 animate-bounce hover:cursor-pointer z-30"
           >
             <ArrowDown size={36} className="text-accent" />
           </button>
@@ -80,6 +98,5 @@ const Hero = () => {
     </section>
   );
 };
-
 
 export default Hero;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,8 +8,8 @@ import Frugalwatt from "./projects/Frugalwatt";
 const Projects = () => {
   return (
     <section id="projects" className="w-full bg-[linear-gradient(to_bottom,rgba(241,241,28,0.15),rgba(241,241,28,0.20))]">
-      <div className="flex flex-row justify-between pt-12">
-        <img src={tiles} className="w-[30%] h-full" />
+      <div className="flex flex-col md:flex-row justify-between pt-12">
+        <img src={tiles} className="w-full md:w-[30%] h-full" />
         <div className="flex flex-row gap-4 items-center justify-between">
           <img src={swork} className="size-24"/>
           <div className="text-center text-accent">
@@ -17,7 +17,7 @@ const Projects = () => {
             <h1 className="text-2xl font-light ">Hand-picked featured projects</h1>
           </div>
         </div>
-        <img src={tiles} className="w-[30%] h-full" />
+        <img src={tiles} className=" w-full md:w-[30%] h-full" />
       </div>
       <div className="py-10">
         <Lensz/>
